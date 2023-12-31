0bb175cb980f56088b528a7edc3df2e2b56d07bb
Syed-Zahaab-Hussain <szahaab32@gmail.com>
2023-12-31 08:08:42 +0500

    Created the frontend and styled it.

--- Password-generator/src/App.jsx
+++ Password-generator/src/App.jsx
@@ -1,35 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from "react";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [password, setPassword] = useState("");
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <div className="container">
+        <div className="generator">
+          <h1 className="generator_heading">Password Generator</h1>
+          <div className="generator_password">
+            <h3>{password}</h3>
+            <button className="copy_btn">
+              <img src="/clipboard.svg" alt="Clipboard icon" />
+            </button>
+          </div>
+
+          <div className="form_group">
+            <label htmlFor="password-strength">Password length</label>
+            <input
+              type="number"
+              id="password-strength"
+              name="password-strength"
+              min="8"
+              max="26"
+            />
+          </div>
+
+          <div className="form_group">
+            <label htmlFor="uppercase-letters">Add Uppercase letters</label>
+            <input
+              type="checkbox"
+              id="uppercase-letters"
+              name="uppercase-letters"
+            />
+          </div>
+
+          <div className="form_group">
+            <label htmlFor="lowercase-letters">Add Lowercase letters</label>
+            <input
+              type="checkbox"
+              id="lowercase-letters"
+              name="lowercase-letters"
+            />
+          </div>
+          
+          <div className="form_group">
+            <label htmlFor="include-numbers">Include numbers</label>
+            <input
+              type="checkbox"
+              id="include-numbers"
+              name="include-numbers"
+            />
+          </div>
+         
+          <div className="form_group">
+            <label htmlFor="include-symbols">Include Symbols</label>
+            <input
+              type="checkbox"
+              id="include-symbols"
+              name="include-symbols"
+            />
+          </div>
+          <button className="generator_btn">Generate Password</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
