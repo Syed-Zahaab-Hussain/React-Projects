5d583666a34877b309c4d9fabf468f46d464a1ce
Syed-Zahaab-Hussain <144938300+Syed-Zahaab-Hussain@users.noreply.github.com>
2024-01-14 22:49:52 +0500

    Created the PopularProduct and Services sections (Components).

---
+++ Nike/src/components/index.js
@@ -0,0 +1,7 @@
+import Nav from "./Nav";
+import Button from "./Button";
+import PopularProductCard from "./PopularProductCard";
+import ShoeCard from "./ShoeCard";
+import ServiceCard from "./ServiceCard";
+
+export { Nav, Button, PopularProductCard, ShoeCard, ServiceCard};
