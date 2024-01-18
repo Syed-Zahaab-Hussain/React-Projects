4f078c12c655883962fc480aa35647c433701fa1
Syed-Zahaab-Hussain <144938300+Syed-Zahaab-Hussain@users.noreply.github.com>
2024-01-18 08:26:41 +0500

    Project Completed

--- Nike/src/components/index.js
+++ Nike/src/components/index.js
@@ -3,5 +3,6 @@ import Button from "./Button";
 import PopularProductCard from "./PopularProductCard";
 import ShoeCard from "./ShoeCard";
 import ServiceCard from "./ServiceCard";
+import ReviewCard from "./ReviewCard";
 
-export { Nav, Button, PopularProductCard, ShoeCard, ServiceCard};
+export { Nav, Button, PopularProductCard, ShoeCard, ServiceCard, ReviewCard };
